bc25a6b7a613bde495f1f20ca0aa572bccc2b5c2
Binary Craft <elliot.huang.signed@gmail.com>
2023-05-31 10:40:18 +0800

    read user input

--- src/main.zig
+++ src/main.zig
@@ -1,31 +1,48 @@
 const std = @import("std");
+const mem = std.mem;
+const io = std.io;
 const Model = @import("Model.zig");
 
 pub fn main() !void {
-    var arena = std.heap.ArenaAllocator.init(
-        std.heap.page_allocator,
-    );
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
 
     var model = try Model.init(.{
-        .mtype = .gptj,
-        .path = "test.model",
-        .threads = 3,
-        .response_fn = responseCallback,
+        .mtype = .mpt,
+        .path = "ggml-mpt-7b-instruct.bin",
+        .threads = 4,
+        .response_fn = struct {
+            fn responseCallback(resp: []const u8) bool {
+                std.debug.print("{s}", .{resp});
+                return true;
+            }
+        }.responseCallback,
     });
     defer model.deinit();
 
     while (true) {
-        // TODO get user input
+        var input = std.ArrayList(u8).init(arena.allocator());
+        defer input.deinit();
+
+        std.debug.print(">>> ", .{});
+        var stdin = io.getStdIn().reader();
+        while (true) {
+            var buf: [4096]u8 = undefined;
+            var line = try stdin.readUntilDelimiterOrEof(&buf, '\n');
+
+            if (line) |l| {
+                if (mem.trim(u8, l, " ").len == 0) {
+                    break;
+                }
+                try input.appendSlice(l);
+                try input.append('\n');
+            }
+        }
+
         var result = try model.predict(.{
-            .text = "123456",
+            .text = input.items,
             .allocator = arena.allocator(),
         });
         defer arena.allocator().free(result);
     }
-}
-
-fn responseCallback(resp: []const u8) bool {
-    std.debug.print("{s}", .{resp});
-    return true;
 }
